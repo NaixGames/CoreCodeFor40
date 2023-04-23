3bfba474a8db822505e7f60875bda5625a59771d
Naix <91845220+Naix99@users.noreply.github.com>
2023-04-23 18:42:59 +0100

    Some namespaces I was missing.

--- GodotLibraryUseExamples/BezierCurves/Scripts/BezierCurveFollowerExample.cs
+++ GodotLibraryUseExamples/BezierCurves/Scripts/BezierCurveFollowerExample.cs
@@ -1,25 +1,27 @@
 using Godot;
 using System;
 
-public partial class BezierCurveFollowerExample : Node2D
-{
-	//Example on how to use BezierCurves. Nothing fancy and not expected to actually be a part of the core library.
-
-	[Export] private Path2D mPathReference;
-	private Curve2D mCurveReference;
-	
-	[Export] private float mSpeed;
-
-	private float mT=0f;
-
-	public override void _Ready(){
-		mCurveReference = mPathReference.Curve;
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+namespace CoreCode.Example{
+	public partial class BezierCurveFollowerExample : Node2D
 	{
-		mT+=mSpeed*((float)delta); 
-		this.Position = mCurveReference.SampleBaked(mT*mCurveReference.GetBakedLength(),true);  //Note this is normalize
+		//Example on how to use BezierCurves. Nothing fancy and not expected to actually be a part of the core library.
+
+		[Export] private Path2D mPathReference;
+		private Curve2D mCurveReference;
+		
+		[Export] private float mSpeed;
+
+		private float mT=0f;
+
+		public override void _Ready(){
+			mCurveReference = mPathReference.Curve;
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+			mT+=mSpeed*((float)delta); 
+			this.Position = mCurveReference.SampleBaked(mT*mCurveReference.GetBakedLength(),true);  //Note this is normalize
+		}
 	}
 }
--- GodotLibraryUseExamples/Tweens/Scripts/TweenUseExample.cs
+++ GodotLibraryUseExamples/Tweens/Scripts/TweenUseExample.cs
@@ -1,28 +1,25 @@
 using Godot;
 using System;
 
-public partial class TweenUseExample : Node
-{
-	//Example on how to use Tweens. Really nice! Be sure to check other Tweeners if needed (as the CallbackTween or the MethodTween)
-	[Export] private Vector2 mObjectivePoint;
-	[Export] private int mTransitionChoice;
-	private Tween mTween;
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+namespace CoreCode.Example{
+	public partial class TweenUseExample : Node
 	{
-		mTween = CreateTween();
-		switch (mTransitionChoice){
-			case (1):
-				mTween.TweenProperty(GetNode(this.GetPath()),"position", mObjectivePoint,2f).AsRelative().SetTrans(Tween.TransitionType.Linear);
-				break;
-			case (2):
-				mTween.TweenProperty(GetNode(this.GetPath()),"position", mObjectivePoint,2f).AsRelative().SetTrans(Tween.TransitionType.Cubic);
-				break;
+		//Example on how to use Tweens. Really nice! Be sure to check other Tweeners if needed (as the CallbackTween or the MethodTween)
+		[Export] private Vector2 mObjectivePoint;
+		[Export] private int mTransitionChoice;
+		private Tween mTween;
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			mTween = CreateTween();
+			switch (mTransitionChoice){
+				case (1):
+					mTween.TweenProperty(GetNode(this.GetPath()),"position", mObjectivePoint,2f).AsRelative().SetTrans(Tween.TransitionType.Linear);
+					break;
+				case (2):
+					mTween.TweenProperty(GetNode(this.GetPath()),"position", mObjectivePoint,2f).AsRelative().SetTrans(Tween.TransitionType.Cubic);
+					break;
+			}
 		}
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
 	}
 }
